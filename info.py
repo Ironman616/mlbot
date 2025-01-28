0a2d6ee99c392000799c08966e436ac8cb3956a4
Ironman616 <150930956+Ironman616@users.noreply.github.com>
2025-01-28 22:12:29 +0530

    Update info.py

--- info.py
+++ info.py
@@ -49,7 +49,7 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Movie_Loverz')
 
 #stream feature - @TeamHMT_Bots
 STREAM_BIN = environ.get("STREAM_BIN", "-1002005906539") #Log channel/Bin Channel id -100xxxxxxx
-STREAM_URL = environ.get("STREAM_URL", "www.movieloverzbotz.us.kg/") # https://example.com/ (ur Stream Url)
+STREAM_URL = environ.get("STREAM_URL", "www.movieloverzbotz.us.kg") # https://example.com/ (ur Stream Url)
 IS_STREAM = bool(STREAM_BIN and STREAM_URL)
 #Use this feature, if you have deployed file to link bot. paste the app url with stream url and add filter bot as admin in bin channel and add id in stream bin
 
